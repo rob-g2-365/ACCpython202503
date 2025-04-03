586ed3f13607431fad4a302ea2e4dba298c175ce
Kyle Clubb <kclubb@austincc.edu>
2025-04-03 00:12:10 +0000

    first file

--- kclubb/test.py
+++ kclubb/test.py
@@ -1,1 +1,18 @@
-print('hello')
+from polygon import RESTClient
+
+client = RESTClient("dIUMbUHa3jguPZ9WiF5HUgIS4FWhPWlq")
+
+aggs = []
+for a in client.list_aggs(
+    "AAPL",
+    1,
+    "day",
+    "2023-01-09",
+    "2023-02-10",
+    adjusted="true",
+    sort="asc",
+    limit=120,
+):
+    aggs.append(a)
+
+print(aggs)
